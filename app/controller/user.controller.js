e2b770239d8875393835db58beea610e1f84b5e6
Noel <noelsoong@live.com>
2019-03-29 18:52:15 +1300

    :zap: Correct Satus Message 201 to 200

--- app/controller/user.controller.js
+++ app/controller/user.controller.js
@@ -1,10 +1,10 @@
-const User = require('../models/users.model');
+const User = require('../models/users.model.js');
 
 exports.readAll = function(req,res) {
     User.listAll(function(result) {
         console.log('user controler');
         if (result['500']){res.status(500); res.json(result)}
-        res.status(201);
+        res.status(200);
         res.json(result);
     });
 };

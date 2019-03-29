50a43a10cad857e115c23e56c118249406ac5759
Noel <noelsoong@live.com>
2019-03-29 18:50:35 +1300

    :zap: wrong table name Users is actually user

--- app/models/users.model.js
+++ app/models/users.model.js
@@ -1,7 +1,9 @@
 const db = require('../../config/db.js');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 exports.listAll = function(done){
-    db.get().query("SELECT * FROM Users", function (err, rows) {
+    db.get().query("SELECT * FROM user", function (err, rows) {
         console.log('user model');
         if (err) {
             return done({"500": "SERVER FAILED REQUEST"})

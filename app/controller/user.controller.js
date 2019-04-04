e7c37c0484ba35a12f3a4b73f4ee60a38f7d7253
Noel <noelsoong@live.com>
2019-04-04 18:08:16 +1300

    :arrow_up: Post to controller handeled

--- app/controller/user.controller.js
+++ app/controller/user.controller.js
@@ -1,10 +1,23 @@
 const User = require('../models/users.model.js');
 
-exports.readAll = function(req,res) {
-    User.listAll(function(result) {
+exports.readAll = function (req, res) {
+    User.listAll(function (result) {
         console.log('user controler');
-        if (result['500']){res.status(500); res.json(result)}
+        if (result['500']) { res.status(500); res.json(result) }
         res.status(200);
         res.json(result);
+    });
+};
+
+
+exports.write = function (req, res) {
+    let userData = req.body;
+    User.create(userData, function (result) {
+        res.setHeader("Content-Type", "application/json");
+        if (result['500']) {
+            res.send({ "errorCode": 500 , "error": result})
+        } else {
+            res.send(201)
+        }
     });
 };

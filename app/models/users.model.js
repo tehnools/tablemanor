0f965c638ea572abc3a9488c1aea853effba21e4
Noel <noelsoong@live.com>
2019-04-04 18:07:05 +1300

    :arrow_up: creation of user data in model

--- app/models/users.model.js
+++ app/models/users.model.js
@@ -1,13 +1,37 @@
+// Local Imports
 const db = require('../../config/db.js');
-const crypto = require('crypto');
+
+// External Imports
+const moment = require('moment');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 exports.listAll = function(done){
-    db.get().query("SELECT * FROM user", function (err, rows) {
+    db.get().query("SELECT * FROM users", function (err, rows) {
         console.log('user model');
         if (err) {
             return done({"500": "SERVER FAILED REQUEST"})
         }
         return done(rows);
+    });
+};
+
+exports.create = function(userData ,done){
+    let sql = "INSERT INTO users (email, name, password, create_time) VALUES ?";
+
+    let query = [[
+        userData.email,
+        userData.name,
+        bcrypt.hashSync(userData.password, 10),
+        moment().unix()
+    ]];
+    
+    console.log(query);
+    db.get().query(sql, [query], function (err, rows) {
+        if (err) {
+            return done({"500": err})
+        } else {
+            return done(rows);
+        }
     });
 };

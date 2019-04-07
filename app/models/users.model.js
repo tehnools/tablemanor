ffcead04a68247ddda2db9d1a6d0f47e4f1a454f
Noel <noelsoong@live.com>
2019-04-07 15:55:58 +1200

    :zap: error is returned instead of json with code and message

--- app/models/users.model.js
+++ app/models/users.model.js
@@ -4,13 +4,35 @@ const db = require('../../config/db.js');
 // External Imports
 const moment = require('moment');
 const bcrypt = require('bcrypt');
+// const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
+
+exports.validatePassword = function (password, hash) {
+    if (bcrypt.compareSync(password, hash)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+exports.generateJWT = function (id, email) {
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+        email: email,
+        id: id,
+        exp: parseInt(expirationDate.getTime() / 1000, 10),
+    }, 'secret');
+}
+
 
 exports.listAll = function (done) {
     db.get().query("SELECT * FROM users", function (err, rows) {
         console.log('user model');
         if (err) {
-            return done({ "500": "SERVER FAILED REQUEST" }, null)
+            return done(err)
         }
         return done(null, rows);
     });
@@ -22,19 +44,17 @@ exports.create = function (userData, done) {
     let query = [[
         userData.email,
         userData.name,
-        bcrypt.hashSync(userData.password, 10),
+        bcrypt.hashSync(userData.password, 10), // hash password
         moment().unix()
     ]];
 
     console.log(query);
     db.get().query(sql, [query], function (err, rows) {
         if (err) {
-            return done({
-                code: 500,
-                message: err
-            }, null)
+            return done(err)
         } else {
             return done();
         }
     });
 };
+

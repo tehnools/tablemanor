16184436a7eca83bfb2c2f5d4ef70cc956a0a334
Noel <noelsoong@live.com>
2019-04-07 15:50:44 +1200

    :arrow_up: Adding passport middleware to config

--- config/express.js
+++ config/express.js
@@ -1,16 +1,30 @@
 // Standard Library Imports
 const express = require('express');
+const cors = require('cors');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 
+// External Imports
+const passport = require('passport');
+
 module.exports = function () {
+  // Init App
   const app = express();
+
+  // Configure Middlewares
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(session({ secret: 'password', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
+  
   // Routes
-  require("../app/routes/user.routes")(app);
-  // require("../app/routes/auth.routes")(app);
+  require("../app/routes/users.routes")(app);
+  require("../app/routes/auth.routes")(app);
+  require('./passport');
+
   // require("../app/routes/home.routes")(app);
 
   return app;

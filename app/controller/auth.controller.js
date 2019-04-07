4dee89eee0af1f59f3d0e1e6b5f76c8bccd2622e
Noel <noelsoong@live.com>
2019-04-07 15:54:39 +1200

    :arrow_up: logout check session

--- app/controller/auth.controller.js
+++ app/controller/auth.controller.js
@@ -8,3 +8,17 @@ exports.login = function (req, res) {
     }
     res.send(201, "Login Sucessful");
 };
+
+
+exports.logout = function (req, res) {
+    console.log("BEFORE" + req.user);
+    req.session.destroy();
+    console.log("AFTER " + req.user);
+    //todo makes sure check if session is destroyed
+    // jwt({secret: process.env.PUBLIC_KEY})
+    // console.log(req.user)
+    // if (!req.user){
+    //     res.send(401, {message: "Email or password is incorrect"})
+    // }
+    // res.send(201, "Login Sucessful");
+};

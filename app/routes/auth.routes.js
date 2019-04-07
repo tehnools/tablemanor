d2ba2ea64b73cb67e5cafec6138b9603f12e2359
Noel <noelsoong@live.com>
2019-04-07 15:51:21 +1200

    :arrow_up: Authorization login complete logout initilised

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,8 +1,12 @@
 const Auth = require("../controller/auth.controller");
+const passport = require("passport");
 
 module.exports = (app) => {
     app.route('/login')
-        .post(Auth.login);
+        // .post(Auth.login)
+        .post(passport.authenticate('local', { failureRedirect: '/login' }),
+            Auth.login
+            );
     app.route('/logout')
-        .post(Auth.logout);
+         .post(Auth.logout);
 }

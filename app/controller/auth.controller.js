48873ecc386dc6cfa9c66aeb5e2d44b4660b1d82
Noel <noelsoong@live.com>
2019-04-25 19:18:58 +1200

    :arrow_up: Refactor token expiry from 5hr to 1d

--- app/controller/auth.controller.js
+++ app/controller/auth.controller.js
@@ -5,7 +5,9 @@ const passport = require('passport');
 const moment = require('moment');
 
 exports.login = function (req, res) {
+    // Check Fields
     if (!req.body.email || !req.body.password) return res.status(401).send('Incomplete Fields');
+
     let sql = "SELECT * FROM Users where email=?";
     db.get().query(sql, [req.body.email], (err, rows) => {
         if (err) { res.status(500).send(err) }
@@ -13,10 +15,9 @@ exports.login = function (req, res) {
         if (result.passport == req.body.passport) {
             const payload = {
                 sub: result.user_id,
-                exp: moment().add(5, 'hours').unix(),
                 iat: moment().unix()
             }
-            const token = jwt.sign(payload, process.env.SECRET_OR_KEY);
+            const token = jwt.sign(payload, process.env.SECRET_OR_KEY,{expiresIn: '1d'});
             res.send(token);
         } else {
             return res.status(400).send()

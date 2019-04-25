6bfa079e8808ac15de08e04c3c1295ae3c82f411
Noel <noelsoong@live.com>
2019-04-25 19:19:43 +1200

    :arrow_up: Refactor logout to correct way

--- app/controller/auth.controller.js
+++ app/controller/auth.controller.js
@@ -25,17 +25,7 @@ exports.login = function (req, res) {
     });
 };
 
-
-
 exports.logout = function (req, res) {
-    console.log("BEFORE" + req.user);
-    req.session.destroy();
-    console.log("AFTER " + req.user);
-    //todo makes sure check if session is destroyed
-    // jwt({secret: process.env.PUBLIC_KEY})
-    // console.log(req.user)
-    // if (!req.user){
-    //     res.send(401, {message: "Email or password is incorrect"})
-    // }
+    req.logout();
     res.send(201, "Logout Sucessful");
 };

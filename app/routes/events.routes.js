6f19237883f37a4c0b53ae0262f2a8c8fd088944
Noel <noelsoong@live.com>
2019-05-20 19:43:29 +1200

    :arrow_up: Comment unused routes

---
+++ app/routes/events.routes.js
@@ -0,0 +1,12 @@
+const Events = require("../controller/events.controller");
+const passport = require("passport");
+
+module.exports = (app) => {
+    app.route('/events/v1/events')
+        // .get(passport.authenticate('jwt', { session: false }), Events.listEvents);
+        .post(passport.authenticate('jwt', { session: false }), Events.createEvent);
+    // app.route('/events/v1/event/:id')
+    //     .get(passport.authenticate('jwt', { session: false }), Events.selectEvent)
+    //     .put(passport.authenticate('jwt', { session: false }), Events.updateEvent)
+    //     .delete(passport.authenticate('jwt', { session: false }), Events.deleteEvent);
+}

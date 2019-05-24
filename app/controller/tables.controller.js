89f3c21b3b38e85717b3fbd5d8be77dd677b20c2
Noel <noelsoong@live.com>
2019-05-24 21:27:52 +1200

    :arrow_up: Can create a table for an event

--- app/controller/tables.controller.js
+++ app/controller/tables.controller.js
@@ -1,1 +1,11 @@
 const Table = require('../models/tables.model.js');
+
+exports.createTable = function (req, res) {
+    Table.create(req.params.eventId, req.body.table, (err) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send();
+        }
+    })
+}
--- app/models/tables.model.js
+++ app/models/tables.model.js
@@ -1,1 +1,18 @@
 const db = require('../../config/db.js');
+
+
+exports.create = function (eventId, data, done) {
+    let sql = "INSERT INTO tables (name, size, eventId) VALUES ?";
+    let values = [[
+        data.name,
+        data.size,
+        eventId
+    ]];
+    db.get().query(sql, [values], (err) => {
+        if (err) {
+            return done(err);
+        } else {
+            return done();
+        }
+    })
+}

4f05d54d148f2add47625f16384081b9e3d60a0c
Noel <noelsoong@live.com>
2019-05-25 15:14:53 +1200

    :arrow_up: Add errors as constants

--- app/models/patrons.model.js
+++ app/models/patrons.model.js
@@ -1,1 +1,5 @@
 const db = require('../../config/db.js');
+
+const errorBadRequestGender = { code: 400, message: "Bad Request - Your request does not fit the requirement 'M' or 'F'" }
+const errorBadRequestFirstName = { code: 400, message: "Bad Request - Missing parameters Firstname" }
+const errorBadRequestContactable = { code: 400, message: "Bad Request - Missing parameters Email when person is Contactble" }
